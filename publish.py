18f5a0dbf156902ea5436028370069c8777e55df
Quimzy <bam.boy2020@gmail.com>
2023-05-06 20:51:14 +0300

    md_info is working

--- publish.py
+++ publish.py
@@ -1,6 +1,7 @@
 import os, shutil
 import markdown
 from collections.abc import Iterable
+import calendar
 
 class Utils:
     @classmethod
@@ -23,19 +24,24 @@ class Convert:
         self.utils = Utils
         self.src_dir = src_dir
         self.dist_dir = dist_dir
-    
-    
-    def compare_content(self):
-        filter_dir = lambda d: self.src_dir not in d and '.' not in d
-        filter_file = lambda f: f[0] != '.' and any(ext in f for ext in ['html', 'md', 'markdown'])
 
-        site_cc = self.utils.flatten([[self.src_dir+'/'+d for d in list(filter(lambda d: '.' not in d, dirs))] + [os.path.join(subdir, f) for f in files] for subdir, dirs, files in os.walk(self.src_dir)])
-
-        dist_cc = [] #current content
+        self.filter_dir = lambda d: self.src_dir not in d and '.' not in d
+        self.filter_file = lambda f: f[0] != '.' and any(ext in f for ext in ['html', 'md', 'markdown'])
+    
+    def get_dist_cc(self):
+        dist_cc = []
         for subdir, dirs, files in os.walk(self.dist_dir):
-            if filter_dir(subdir[2:]):
-                dist_cc.extend(self.utils.flatten([self.dist_dir+'/'+d for d in list(filter(filter_dir, dirs))] + [os.path.join(subdir, os.path.splitext(f)[0]) for f in list(filter(filter_file, files))]))     
+            if self.filter_dir(subdir[2:]):
+                dist_cc.extend(self.utils.flatten([subdir+'/'+d for d in list(filter(self.filter_dir, dirs))] + [os.path.join(subdir, os.path.splitext(f)[0]) for f in list(filter(self.filter_file, files))]))     
         
+        return dist_cc
+
+    def compare_content(self):
+        
+        site_cc = self.utils.flatten([[subdir+'/'+d for d in list(filter(lambda d: '.' not in d, dirs))] + [os.path.join(subdir, f) for f in files] for subdir, dirs, files in os.walk(self.src_dir)])
+
+        dist_cc = self.get_dist_cc() #current content
+                
         return [c for c in site_cc if self.utils.replace_md(c.replace('site/', './')) not in dist_cc] +\
                 [c for c in dist_cc if c.replace('./', 'site/') not in [self.utils.replace_md(x) for x in site_cc]]
     
@@ -48,6 +54,7 @@ class Convert:
 
     def convert(self):
         dist_content = self.compare_content()
+        
         for content in dist_content:    
             dist_path = self.utils.replace_md(content.replace(f'{self.src_dir}/', ''))
             if os.path.isdir(content):
@@ -62,6 +69,38 @@ class Convert:
             elif not os.path.isdir(content) and os.path.isfile(dist_path+'.html'): 
                 os.remove(dist_path+'.html')
 
+class Publish:
+    def __init__(self, src_dir="site", dist_dir=".", posts_dir="bible/posts/"):
+        self.src_dir = src_dir
+        self.dist_dir = dist_dir
+        self.posts_dir = posts_dir
+        
+        self.convert = Convert(src_dir, dist_dir)
+        self.convert.convert()
+    
+    def md_info(self, filename="2022-03-22-who-am-i"): #extract article title and date and turn them into markdown
+        parts = filename.split('-')
+
+        date = f"{calendar.month_name[int(parts[1])]} {parts[2]}, {parts[0]}" 
+        name = " ".join([w.capitalize() for w in parts[3:]])
+        link = os.path.join(self.posts_dir, filename+".html")
+
+        md = f'<span style="font-size: 14px; color: #828282;"> *{date}*</span>\n###[{name}](/{link})\n<br/>'
+        return md
+    
+    def sort_posts(self):
+        posts = os.listdir(self.posts_dir)
+        pass
+
+    def update_feed(self):
+        sorted_posts = sorted_posts
+ 
+        for file in sorted_posts:
+            info = self.md_info(self.convert.utils.replace_md(file))
+            print(info)
+
+    
+        
 if __name__ == '__main__':
-    convert = Convert()
-    convert.convert()
+    convert = Publish()
+    convert.update_feed()
